2e182526b61b5d3deedd7dcebc3f5b1500f3a65f
Saurabh Yadav <saurabhyadav311998@gmail.com>
2022-02-23 19:05:28 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
     function RenderDish({dish}) {
 
@@ -66,17 +68,28 @@ import { Card, CardImg, CardText, CardBody,
             return (<div></div>)
         }
 
-        //const dishItem = <RenderDish dish={props.dish}/>;
-        //const dishComment = <RenderComments comments={props.dish.comments} />
+        const dishItem = <RenderDish dish={props.dish}/>;
+        const dishComment = <RenderComments comments={props.dish.comments} />
 
         return (
             <div className="container">
-                <div className='row'>
-                    <RenderDish dish={ props.dish }/>
-                    <RenderComments comments={ props.dish.comments } />
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            
+            <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
-        )
-    }
-
+        );
+        }
 export default DishDetail; 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,14 +3,17 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -38,13 +41,31 @@ class Main extends Component {
       );
     }
 
+    const AboutUsPage = () => {
+      return(
+          <About 
+              leaders={this.state.leaders}
+          />
+      );
+  };
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
+
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path="/aboutus" component={ AboutUsPage } />
               <Redirect to="/home" />
           </Switch>
         <Footer />
