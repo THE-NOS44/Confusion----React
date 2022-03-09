3b985a567840be1f73ff0b061d7e589f2c233935
Saurabh Yadav <saurabhyadav311998@gmail.com>
2022-03-09 21:34:06 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,7 @@ import {Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from "reactstrap
 
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 /**........................ comment component ends ................................................. */
@@ -173,7 +173,7 @@ class CommentForm extends Component {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
